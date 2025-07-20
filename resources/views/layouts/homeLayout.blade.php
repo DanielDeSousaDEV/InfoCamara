ced273fbcf0a9c11a9587f9454709c9f522ccc09
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-07-20 09:11:03 -0300

    chore: removing the use of CND´s

--- resources/views/layouts/homeLayout.blade.php
+++ resources/views/layouts/homeLayout.blade.php
@@ -15,6 +15,6 @@
     @yield('content')
 
     @stack('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    <script src="../../node_modules/flowbite/dist/flowbite.min.js"></script>
 </body>
 </html>
