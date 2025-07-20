665f482f98ab21434fb0b9c579d2130ed1ba1f1c
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-07-20 09:10:42 -0300

    chore: locale date-fns

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
+import { setDefaultOptions } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['Accept'] = 'application/json';
+
+setDefaultOptions({ locale: ptBR })
