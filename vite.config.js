79f984cc2339797bfcc112d32529d80e48306033
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-07-24 16:07:57 -0300

    chore: adding the vite url in the .env file

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,6 @@ export default defineConfig({
     server: {
         host: process.env.VITE_HOST || '0.0.0.0',
         port: Number(process.env.VITE_PORT) || 5173,
+        origin: process.env.VITE_URL || 'http://localhost:5173', 
     }
 });
